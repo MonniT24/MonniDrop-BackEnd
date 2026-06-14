d914c8d5a3c7ddda0eef79f4e12a1d4de3006303
Monni Boss <monniboss70@gmail.com>
2026-06-14 13:01:15 +0000

    Emit new order event to riders

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -585,6 +585,15 @@ exports.updateOrder =
 
       await order.save();
 
+      const io = req.app.get("io");
+
+if(io){
+  io.emit("newOrderForRiders", {
+    message:"New delivery request available",
+    order
+  });
+}
+
       const updated =
         await Order.findById(
           order._id
